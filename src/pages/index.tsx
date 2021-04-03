be5a41baba3ae3fc27acf168ec9feda45c68f3e7
Percegaroli <bruno.percegaroli@gmail.com>
2021-04-03 14:41:00 -0300

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <div>
+        Hello World
+      </div>
+    </div>
+  );
+}
